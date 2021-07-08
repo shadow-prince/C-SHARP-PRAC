a776ca6f28ede7f8f61373b85a15d53c17f6abf9
shadow-prince <59025976+shadow-prince@users.noreply.github.com>
2021-07-08 22:02:35 +0530

    init commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+namespace C_SHARP_PRAC
+{
+	class Program
+	{
+
+		static void Display(int i)
+		{
+			Console.WriteLine("Param passed : " + i);
+		}
+
+		static void printForTen(int j)
+		{
+			for (int i = 1; i < j + 1; i++)
+			{
+				Console.WriteLine(i);
+
+			}
+		}
+
+		static void displayName(string name, int age)
+		{
+			Console.WriteLine("Name :" + name + ", Age: " + age);
+		}
+
+		static void MyMethod(string child1 = "Liam", string child2 = "Jenny", string child3 = "John")
+		{
+			Console.WriteLine("The youngest child is: " + child3);
+		}
+		static void Main(string[] args)
+		{
+
+			//var i = 10;
+			newclass nc = new newclass();
+			Console.WriteLine(value: nc.color);
+		}
+	}
+}
---
+++ newclass.cs
@@ -0,0 +1,7 @@
+namespace C_SHARP_PRAC
+{
+    public class newclass
+    {
+        public string color = "red";
+    }
+}
